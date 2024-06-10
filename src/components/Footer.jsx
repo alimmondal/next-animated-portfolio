38b787f833a5170cd326474a138766c957cf2f07
alimmondal <italimbd@gmail.com>
2024-06-10 14:26:30 +0600

    btn upd

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,12 +21,54 @@ const Footer = () => {
       <Image src={Wave} alt="" className="w-full h-full" />
 
       <div className={styles.fContent}>
-        <div className="text-slate-500 flex items-center justify-center gap-2 hover:text-red-700 mt-20 md:mt-40 md:pb-5">
-          {links.map((link) => (
+        <div className="text-slate-500 flex items-center justify-center gap-2 mt-20 md:mt-40 md:pb-5">
+          {/* {links.map((link) => (
             <Link key={link?.title} href={link.url}>
               <p className="text-xl">{link.title}</p>
             </Link>
-          ))}
+          ))} */}
+          <ul className="flex gap-4 ">
+            <li>
+              <Link
+                href={"/"}
+                className="sm:text-[22px] hover:underline hover:sm:text-rose-700 "
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={"/about"}
+                className="sm:text-[22px] hover:underline hover:sm:text-rose-700 "
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={"/contact"}
+                className="sm:text-[22px] hover:underline hover:sm:text-rose-700 "
+              >
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={"/portfolio"}
+                className="sm:text-[22px] hover:underline hover:text-rose-700 "
+              >
+                Portfolio
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={"/blog"}
+                className="sm:text-[22px] hover:underline hover:text-rose-700 "
+              >
+                Blog
+              </Link>
+            </li>
+          </ul>
         </div>
         <div className={styles.fIcons}>
           <SocialLinks />
